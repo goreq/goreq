a1deb7afbda4e35a6deaac2d9cb7bb1e2f2b7efd
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2022-01-11 18:44:15 +0700

    docs(example): add simple usage

---
+++ example/simple/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hadihammurabi/gore"
+)
+
+func panicOnError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	g := gore.New(
+		gore.WithBaseURL("https://my-json-server.typicode.com/hadihammurabi/flutter-webservice"),
+	)
+
+	resp, err := g.Get("/contacts", nil)
+	panicOnError(err)
+	defer resp.Body.Close()
+
+	data := []interface{}{}
+	err = resp.Json(&data)
+	panicOnError(err)
+
+	fmt.Println(data)
+}

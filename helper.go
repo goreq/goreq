4fa63eb15970cf759fa63edc5921f0266610c597
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2022-10-04 22:03:35 +0700

    fix(request): remove header from helper parameter

--- helper.go
+++ helper.go
@@ -1,25 +1,21 @@
 package goreq
 
-import (
-	"net/http"
-)
-
 func Get(url string, opts ...Option) (*Response, error) {
 	return New(opts...).Get(url)
 }
 
-func Post(url string, header http.Header, opts ...Option) (*Response, error) {
+func Post(url string, opts ...Option) (*Response, error) {
 	return New(opts...).Post(url, opts...)
 }
 
-func Put(url string, header http.Header, opts ...Option) (*Response, error) {
+func Put(url string, opts ...Option) (*Response, error) {
 	return New(opts...).Put(url, opts...)
 }
 
-func Patch(url string, header http.Header, opts ...Option) (*Response, error) {
+func Patch(url string, opts ...Option) (*Response, error) {
 	return New(opts...).Patch(url, opts...)
 }
 
-func Delete(url string, header http.Header, opts ...Option) (*Response, error) {
+func Delete(url string, opts ...Option) (*Response, error) {
 	return New(opts...).Delete(url, opts...)
 }

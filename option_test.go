946ca2661ad3d803e7dc7c2cbef49165b00db76a
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2022-10-05 23:18:08 +0700

    feat(option): test resolve options

--- option_test.go
+++ option_test.go
@@ -54,3 +54,48 @@ func TestOptions(t *testing.T) {
 	must.NotNil(c.errorHandler)
 
 }
+
+func TestResolveOptions(t *testing.T) {
+	var (
+		expectedTimeout              = 1 * time.Second
+		expectedBaseUrl              = "http://127.0.0.1/"
+		expectedBaseHeader           = http.Header{"test": {"true"}}
+		expectedBody                 = []byte("test")
+		expectedErrHandler           = ErrorHandler(func(err error) {})
+		expectedBeforeRequestHandler = BeforeRequestHandler(func(req *http.Request) {})
+		expectedAfterRequestHandler  = AfterResponseHandler(func(resp *http.Response) {})
+	)
+
+	c := &client{}
+	must := must.New(t)
+
+	resolveOptions(
+		c,
+		WithTimeout(expectedTimeout),
+		WithBaseURL(expectedBaseUrl),
+		WithBaseHeader(expectedBaseHeader),
+		WithHeader(expectedBaseHeader),
+		WithBody(expectedBody),
+		WithBeforeRequestHandler(expectedBeforeRequestHandler),
+		WithAfterResponseHandler(expectedAfterRequestHandler),
+		WithHeader(expectedBaseHeader),
+		WithErrorHandler(expectedErrHandler),
+	)
+
+	must.Equal(c.timeout, expectedTimeout)
+	must.Equal(c.baseURL, expectedBaseUrl)
+	must.Equal(c.temporaryBody, expectedBody)
+	must.NotNil(c.beforeRequestHandler)
+	must.NotNil(c.afterResponseHandler)
+	must.NotNil(c.errorHandler)
+
+	newHeader := make(http.Header)
+	for key, val := range expectedBaseHeader {
+		newHeader[key] = val
+	}
+	for key, val := range expectedBaseHeader {
+		newHeader[key] = val
+	}
+	must.Equal(c.baseHeader, newHeader)
+
+}

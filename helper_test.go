cb6be872389fae08abfd411c3a2e04712645951b
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2022-10-05 22:54:32 +0700

    feat(helper): test all method

--- helper_test.go
+++ helper_test.go
@@ -19,7 +19,79 @@ func TestGet(t *testing.T) {
 	must := must.New(t)
 
 	g := New()
-	resp, err := g.Get(svr.URL, nil)
+	resp, err := g.Get(svr.URL)
+
+	must.Nil(err)
+	defer resp.Body.Close()
+
+	must.Equal(resp.String(), expected)
+}
+
+func TestPost(t *testing.T) {
+	expected := "test data"
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, expected)
+	}))
+	defer svr.Close()
+
+	must := must.New(t)
+
+	g := New()
+	resp, err := g.Post(svr.URL)
+
+	must.Nil(err)
+	defer resp.Body.Close()
+
+	must.Equal(resp.String(), expected)
+}
+
+func TestPut(t *testing.T) {
+	expected := "test data"
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, expected)
+	}))
+	defer svr.Close()
+
+	must := must.New(t)
+
+	g := New()
+	resp, err := g.Put(svr.URL)
+
+	must.Nil(err)
+	defer resp.Body.Close()
+
+	must.Equal(resp.String(), expected)
+}
+
+func TestPatch(t *testing.T) {
+	expected := "test data"
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, expected)
+	}))
+	defer svr.Close()
+
+	must := must.New(t)
+
+	g := New()
+	resp, err := g.Patch(svr.URL)
+
+	must.Nil(err)
+	defer resp.Body.Close()
+
+	must.Equal(resp.String(), expected)
+}
+
+func TestDelete(t *testing.T) {
+	expected := "test data"
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, expected)
+	}))
+	defer svr.Close()
+
+	must := must.New(t)
+
+	g := New()
+	resp, err := g.Delete(svr.URL)
 
 	must.Nil(err)
 	defer resp.Body.Close()
